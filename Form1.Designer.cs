39db90ca3ba915096dfae81ba4e8a96c4c8ded8b
Serhat Yatar <srhtytr7@gmail.com>
2019-06-10 08:58:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace udp_synchronous
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_ServerStart = new System.Windows.Forms.Button();
+            this.button_client1Send = new System.Windows.Forms.Button();
+            this.button_client2Send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(70, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(325, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Client1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(325, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Client2";
+            // 
+            // button_ServerStart
+            // 
+            this.button_ServerStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_ServerStart.Location = new System.Drawing.Point(34, 336);
+            this.button_ServerStart.Name = "button_ServerStart";
+            this.button_ServerStart.Size = new System.Drawing.Size(149, 49);
+            this.button_ServerStart.TabIndex = 3;
+            this.button_ServerStart.Text = "Start Server";
+            this.button_ServerStart.UseVisualStyleBackColor = true;
+            this.button_ServerStart.Click += new System.EventHandler(this.button_ServerStart_Click);
+            // 
+            // button_client1Send
+            // 
+            this.button_client1Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_client1Send.Location = new System.Drawing.Point(281, 125);
+            this.button_client1Send.Name = "button_client1Send";
+            this.button_client1Send.Size = new System.Drawing.Size(152, 49);
+            this.button_client1Send.TabIndex = 4;
+            this.button_client1Send.Text = "Send";
+            this.button_client1Send.UseVisualStyleBackColor = true;
+            this.button_client1Send.Click += new System.EventHandler(this.button_client1Send_Click);
+            // 
+            // button_client2Send
+            // 
+            this.button_client2Send.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_client2Send.Location = new System.Drawing.Point(281, 281);
+            this.button_client2Send.Name = "button_client2Send";
+            this.button_client2Send.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button_client2Send.Size = new System.Drawing.Size(152, 49);
+            this.button_client2Send.TabIndex = 4;
+            this.button_client2Send.Text = "Send";
+            this.button_client2Send.UseVisualStyleBackColor = true;
+            this.button_client2Send.Click += new System.EventHandler(this.button_client2Send_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(281, 255);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(281, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(34, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(149, 290);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 436);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_client2Send);
+            this.Controls.Add(this.button_client1Send);
+            this.Controls.Add(this.button_ServerStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_ServerStart;
+        private System.Windows.Forms.Button button_client1Send;
+        private System.Windows.Forms.Button button_client2Send;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
