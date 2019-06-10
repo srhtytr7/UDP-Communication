91e92b196cee1a3b0df013ee1c64bb83f682debc
Serhat Yatar <srhtytr7@gmail.com>
2019-06-10 09:06:13 +0300

    Add files via upload

---
+++ synchronous/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace udp_synchronous
+{
+    static class Program
+    {
+        public static Form1 mainForm = null;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            mainForm = new Form1();
+            Application.Run(mainForm);
+        }
+    }
+}
